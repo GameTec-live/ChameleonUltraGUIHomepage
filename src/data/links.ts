e65d896f688e81efc8e8db880683ba86cd8b9032
GameTec_live <benedikt.werner08@gmail.com>
2025-09-08 20:07:42 +0200

    feat: website

---
+++ src/data/links.ts
@@ -0,0 +1,23 @@
+export const GITHUB_REPO = "https://github.com/GameTec-live/ChameleonUltraGUI";
+export const RELEASES_URL = `${GITHUB_REPO}/releases`;
+
+export const DOWNLOADS = {
+    windows: `${RELEASES_URL}/download/dev/windows-installer.zip`,
+    windowsPortable: `${RELEASES_URL}/download/dev/windows.zip`,
+    macos: "https://apps.apple.com/app/chameleon-ultra-gui/id6462919364",
+    linux: `${RELEASES_URL}/download/dev/linux.zip`,
+    linuxLegacy: `${RELEASES_URL}/download/dev/linux-legacy.zip`,
+    linuxDeb: `${RELEASES_URL}/download/dev/linux-debian.zip`,
+    linuxArch: "https://aur.archlinux.org/packages/chameleonultragui-git",
+    android: "https://play.google.com/store/apps/details?id=io.chameleon.ultra",
+    androidAPK: `${RELEASES_URL}/download/dev/apk.zip`,
+    ios: "https://apps.apple.com/app/chameleon-ultra-gui/id6462919364",
+};
+
+export const SOCIAL = {
+    github: GITHUB_REPO,
+};
+
+export const DONATE = {
+    openCollective: "https://opencollective.com/chameleon-ultra-gui",
+};
